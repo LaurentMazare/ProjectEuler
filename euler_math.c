7d6829d2d6cb69a541f544d07a4ee6f2399a7f73
Laurent Mazare <laurent.mazare@gmail.com>
2012-08-03 22:22:18 +0100

    Add a few functions (untested for now).

--- euler_math.c
+++ euler_math.c
@@ -22,3 +22,46 @@ int* get_primes(int m, int* nb_primes_out) {
   printf("Sieve finished: %d primes found!\n", nb_primes);
   return primes;
 }
+
+int primality_check(int x, int* primes, int nb_primes) {
+  if (x <= primes[nb_primes-1]) {
+    // Bisection search
+    int lo = 0;
+    int up = nb_primes - 1;
+    while ((up - lo) > 1) {
+      int mid = (lo + up) / 2;
+      if (primes[mid] == x)
+        return 1;
+      if (primes[mid] < x)
+        lo = mid;
+      else
+        up = mid;
+    }
+    return (primes[lo] == x || primes[up] == x);
+  }
+  for (int i = 0; i < nb_primes; i++) {
+    if (x == i) // Should never happen
+      return 1;
+    if (x % i == 0)
+      return 0;
+  }
+  return 1;
+}
+
+int gcd_int(int a, int b) {
+  while (b != 0) {
+    int tmp = b;
+    b = a % b;
+    a = tmp;
+  }
+  return a;
+}
+
+llu gcd_llu(llu a, llu b) {
+  while (b != 0) {
+    llu tmp = b;
+    b = a % b;
+    a = tmp;
+  }
+  return a;
+}
--- euler_math.h
+++ euler_math.h
@@ -6,6 +6,17 @@
 typedef long long int lli;
 typedef unsigned long long int llu;
 
+// Prime sieve: get_primes(max_p, &nb_primes)
+// return an array with nb_primes elements containing
+// all the primes between 2 and max_p (inclusive).
 int* get_primes(int, int*);
+
+// primality_check(x, primes, nb_primes)
+// x has to be lower than the square of the largest prime
+int primality_check(int, int*, int);
+
+// GCD (using Euclide algorithm)
+int gcd_int(int, int);
+llu gcd_llu(llu, llu);
 
 #endif
