7bc449675e9b17ebe3e6c942a2bd26c44f4186f5
Laurent Mazare <laurent.mazare@gmail.com>
2012-08-16 06:29:23 +0100

    Another improvement.

--- e139.cpp
+++ e139.cpp
@@ -6,10 +6,9 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  int nb = 0;
   set< long long int > all_pairs;
-  for (int m = 0; m < 10000; m++) {
-    for (int n = 0; n < m; n++) {
+  for (int m = 1; m < 10000; m++) {
+    for (int n = 1; n < m; n++) {
       int a = m * m - n * n;
       int b = 2 * m * n;
       int c = m * m + n * n;
@@ -22,17 +21,15 @@ int main(int argc, char* argv[]) {
         cc += c;
         int min = aa < bb ? aa: bb;
         int max = aa < bb ? bb: aa;
-        long long int p = (long long int)min * MAXL + (long long int)max;
-        if (all_pairs.find(p) == all_pairs.end()) {
+        if (cc % (max - min) == 0) {
+          long long int p = (long long int)min * MAXL + (long long int)max;
           all_pairs.insert(p);
-          if (cc % (max - min) == 0)
-            nb++;
         }
       }
       if (a + b + c >= MAXL)
         break;
     }
   }
-  printf("%d\n", nb);
+  printf("%d\n", all_pairs.size());
   return 0;
 }
